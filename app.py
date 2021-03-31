d695368e21f38da7085ba7810e1b0f3df015b6de
rafaeldiez <rafael.diez@gmail.com>
2021-03-31 14:14:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print("Handling request to home page.")
-    return "Hello, Azure with git test 2!"
+    return "Hello, Azure with git integration!"
